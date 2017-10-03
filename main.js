ace53c3fac6a65cf5d84223489e18f48671e0596
Benjamin White <32458896+BenWhite89@users.noreply.github.com>
2017-10-03 12:01:49 -0500

    day 2 lab

---
+++ main.js
@@ -0,0 +1,11 @@
+var friends = [`Vanessa`, `Gabriel`, `Heather`, `Otto`, `Mark`];
+
+for (j = 0; j < 4; j++) {
+
+  for (i = 99; i > 2; i--) {
+    console.log(`${i} lines of code in the file, ${i} lines of code; ${friends[j]} strikes one out, clears it all out, ${i-1} lines of code in the file.`);
+  };
+  console.log(`2 lines of code in the file, 2 lines of code; ${friends[j]} strikes one out, clears it all out, 1 line of code in the file.`);
+  console.log(`1 line of code in the file, 1 line of code; ${friends[j]} strikes one out, clears it all out, no more lines of code in the file.`);
+
+};
