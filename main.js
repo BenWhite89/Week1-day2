d00e1301fe6f6bd2d2c351ae0379ead83e0cc121
Benjamin White <32458896+BenWhite89@users.noreply.github.com>
2017-10-04 10:17:50 -0500

    Add name heading

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var friends = [`Vanessa`, `Gabriel`, `Heather`, `Otto`, `Mark`];
     
 
 for (j = 0; j < friends.length; j++) {
+  console.log(friends[j]);
   
   for (i = 99; i > 2; i--) {
     console.log(`${i} lines of code in the file, ${i} lines of code; ${friends[j]} strikes one out, clears it all out, ${i-1} lines of code in the file.`);
